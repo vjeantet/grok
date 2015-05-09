0c5bd34c063aac8f9e8d22f06c9f622f73c544d6
Valere JEANTET <valere.jeantet@gmail.com>
2015-05-09 22:43:35 +0200

    grok.AddPattern example added

--- example/main.go
+++ example/main.go
@@ -20,4 +20,15 @@ func main() {
 	for k, v := range values {
 		fmt.Printf("%+15s: %s\n", k, v)
 	}
+
+	fmt.Println("\n\n# Add custom patterns :\n")
+	// We add 3 patterns to our Grok instance, to structure an IRC message
+	g = grok.New(grok.NAMEDCAPTURE)
+	g.AddPattern("IRCUSER", `\A@(\w+)`)
+	g.AddPattern("IRCBODY", `.*`)
+	g.AddPattern("IRCMSG", `%{IRCUSER:user} .* : %{IRCBODY:message}`)
+	values, _ = g.Parse("%{IRCMSG}", `@vjeantet said : Hello !`)
+	for k, v := range values {
+		fmt.Printf("%+15s: %s\n", k, v)
+	}
 }
